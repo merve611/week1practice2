6456056e8d074ace30c26b437c8fe003d506faca
merve cakiroglu <mrveeckrrr@hotmail.com>
2024-07-18 10:54:54 +0300

    Add project files.

---
+++ week1practice2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+int sayi;
+
+Console.Write("Lütfen bir sayi giriniz: ");
+sayi = Convert.ToInt32(Console.ReadLine());
+
+if (sayi == 10)
+{
+    Console.WriteLine(" sayi 10'a eşittir");
+}
+else if (sayi < 10)
+{
+    Console.WriteLine("Sayi 10'dan küçüktür");
+}
+else
+{
+    Console.WriteLine("Sayi 10 dan büyüktür");
+}
+
+
+if (sayi % 2 ==0)
+{
+    Console.WriteLine("Sayi çifttir");
+}
+else
+{
+    Console.WriteLine("sayi tektir");
+}
